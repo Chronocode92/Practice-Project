90f5744576ddb809036459af05305e40f167ed20
Chronocode92 <78360259+Chronocode92@users.noreply.github.com>
2023-03-27 16:00:02 +0100

    added new buttons
    Added buttons for moving left and right

--- Practice project - unity/Assets/Scripts/PlayerMovement.cs
+++ Practice project - unity/Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
 {
+    // speed setting, in meters/second
+    public float speed = 1;
+    // jump speed setting, in meters/second
+    public float jumpSpeed = 10;
     // Reference variable so we can use the Rigidbody
     // Set it to null by default, which means we do NOT have a reference yet.
     private Rigidbody2D physicsBody = null;
@@ -11,6 +15,39 @@ public class PlayerMovement : MonoBehaviour
     private void Awake()
     {
         physicsBody = GetComponent < Rigidbody2D>();
+    }
+
+    public void MoveLeft()
+    {
+        // make a variable to hold our velocity and get the 
+        // curent velocity from the physics component
+        Vector2 newVelocity = physicsBody.velocity;
+        // Set our velocity to move in the negative x (left) direction
+        newVelocity.x = -speed;
+        // Update our physics component's velocity to be our newly changed value
+        physicsBody.velocity = newVelocity;
+    }
+
+    public void MoveRight()
+    {
+        // make a variable to hold our velocity and get the 
+        // curent velocity from the physics component
+        Vector2 newVelocity = physicsBody.velocity;
+        // Set our velocity to move in the positive x (right) direction
+        newVelocity.x = +speed;
+        // Update our physics component's velocity to be our newly changed value
+        physicsBody.velocity = newVelocity;
+    }
+
+    public void jump()
+    {
+        // make a variable to hold our velocity and get the
+        // current velocity from the physics component
+        Vector2 newVelocity = physicsBody.velocity;
+        // Set our velocity to move in the positive y (up) direction
+        newVelocity.y = jumpSpeed;
+        //Update our physics component's velocity to be our newly changed value
+        physicsBody.velocity = newVelocity;
     }
 
     // Update is called once per frame
